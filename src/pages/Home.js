4c005d3a1426d435c9aa0f35c80e9ffbe6f9b3bd
Marco Romero <46950374+mar807@users.noreply.github.com>
2023-05-18 23:43:22 -0700

    started working on the introduction of the page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,24 @@
 import React from "react";
+import "../styles/Home.css";
 
 function Home(){
     return(
-      <h1>Test</h1>  
+      <div className="home-page">
+        <div className="first-section">
+          <h2>Welcome to Next Meal!</h2>
+          <h3>Enter ingredients here, and click enter to start adding your list of ingredients!</h3>
+          <input className="ingredients-text-box" type="text" placeholder="Enter your ingredients!"/>
+          <button className="search-button">Search</button>
+          <p>this is all subject to change once i find my notes</p>
+        </div>
+        <div className="second-section">
+          <p>this is where the rotating foods are gonna be. it will be a random selection on a caraousel.</p>
+        </div>
+        <div className="third-section">
+          <p>this is i think where i put the about us or something. i have to check my notes.</p>
+          <p>im just putting the notes here to structure how the page is going to look</p>
+        </div>
+      </div>
     );
 };
 
