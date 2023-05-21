302678698981484ffc54e9ced10b77927cef4d7d
Marco <marco.a.romero0303@gmail.com>
2023-05-20 19:40:49 -0700

    finished up the logic for adding text under the input box. disabled the add button when there is 8 text. changed directions

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import "../../styles/NavBar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch, faTimes } from "@fortawesome/free-solid-svg-icons";
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,53 @@
-import React from "react";
+import React, { useState} from "react";
 import "../styles/Home.css";
-import { useState } from "react";
 
 function Home(){
   const [searchValue, setSearchValue] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   const searchInputHandle = (event) => {
     setSearchValue(event.target.value);
   };
 
-  const searchHandle = () => {
-    console.log("Searched ingredient was: " + searchValue);
+  const addHandle = () => {
+    setSearchResults(prevResults => [...prevResults, searchValue]);
     setSearchValue('');
   };
+
+  const searchHandle = () => {
+    console.log("Searched ingredient was: " + searchResults);
+    setSearchValue('');
+  };
+
+  const clearHandle = () => {
+    setSearchResults([]);
+  }
 
     return(
       <div className="home-page">
         <div className="first-section">
           <div className="introduction-verbiage">
             <h2>Welcome to Next Meal!</h2>
-            <h3>Enter ingredients here, and click enter to start adding your list of ingredients!</h3>
+            <h3>Enter ingredients here, and add to start adding your list of ingredients!</h3>
           </div>
           <div className="search-food">
-            <input className="ingredients-text-box" value={searchValue} onChange={searchInputHandle} type="text" placeholder="Enter your ingredients!"/>
-            <button className="button-home add">Add</button>
-            <button className="button-home search" onClick={searchHandle}>Search</button>
-            <button className="button-home clear">Clear Filters</button>
+            <div className="search-food-buttons">
+              {searchResults.length <= 8 && (
+                <input className="ingredients-text-box" value={searchValue} onChange={searchInputHandle} type="text" placeholder="Enter your ingredients! (Max of 8 Ingredients)"/>)}
+              {searchResults.length <= 8 && (<button className="button-home add" onClick={addHandle}>Add</button>) }
+              {searchResults.length > 8 && (
+                <input className="ingredients-text-box max" type="text" placeholder="Reached Max Ingredients" readOnly/>
+              )}
+              <button className="button-home search" onClick={searchHandle}>Search</button>
+              <button className="button-home clear" onClick={clearHandle}>Clear Filters</button>
+            </div>
+            <div className="list-ingredients">
+              <ul>
+                {searchResults.map((result, index) => (
+                  <li className="search-results" key={index}>{result}</li>
+                ))}
+              </ul>
+            </div>
           </div>
           {/*<p>this is all subject to change once i find my notes</p>*/}
         </div>
