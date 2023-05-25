587045b53e22b6ee98077d7cc6e2e5cf8867f78a
Marco Romero <46950374+mar807@users.noreply.github.com>
2023-05-25 01:06:13 -0700

    continueing work on the carasouel for the home page

--- src/components/homepage/Caraousel.js
+++ src/components/homepage/Caraousel.js
@@ -1,44 +1,57 @@
 import React, { useState } from "react";
 import "../../styles/homepage/Caraousel.css";
 
-function Caraousel(){
+function Caraousel({ slides }){
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const goToNextSlide = () => {
+    setCurrentSlide((currentSlide + 1) % slides.length);
+  };
+
+  const goToPreviousSlide = () => {
+    setCurrentSlide((currentSlide - 1 + slides.length) % slides.length);
+  };
+
     return (
-        <div className="second-section">
-          <h3 className="second-section-header">Popular Foods Of The Day</h3>
-          <div className="popular-foods">
-            <div className="popular-food-arrows left">
-              <div></div>
+      <div className="second-section">
+        <h3 className="second-section-header">Popular Foods Of The Day</h3>
+        <div className="popular-foods">
+          <div className="carousel">
+            <div className="carousel-wrapper">
+              {slides.map((slide, index) => (
+                <div className={`slide ${index === currentSlide ? 'active' : ''}`} key={index}>
+                  <div className="popular-food-item">
+                    <div className="popular-food-pic">
+                          <div></div>
+                    </div>
+                    <div className="popular-food-name">
+                      <span>testing</span>
+                    </div>
+                  </div>
+                  <div className="popular-food-item">
+                    <div className="popular-food-pic">
+                      <div></div>
+                    </div>
+                    <div className="popular-food-name">
+                      <span>testing</span>
+                    </div>
+                  </div>
+                  <div className="popular-food-item">
+                    <div className="popular-food-pic">
+                      <div></div>
+                    </div>
+                    <div className="popular-food-name">
+                      <span>testing</span>
+                    </div>
+                  </div>
+                </div>
+              ))}
             </div>
-            <div className="popular-food-item">
-              <div className="popular-food-pic">
-                    <div></div>
-              </div>
-              <div className="popular-food-name">
-                <span>testing</span>
-              </div>
-            </div>
-            <div className="popular-food-item">
-              <div className="popular-food-pic">
-                <div></div>
-              </div>
-              <div className="popular-food-name">
-                <span>testing</span>
-              </div>
-            </div>
-            <div className="popular-food-item">
-              <div className="popular-food-pic">
-                <div></div>
-              </div>
-              <div className="popular-food-name">
-                <span>testing</span>
-              </div>
-            </div>
-            <div className="popular-food-arrows right">
-              <div></div>
-            </div>
+            <button className="prev-button" onClick={goToPreviousSlide}>Previous</button>
+            <button className="next-button" onClick={goToNextSlide}>Next</button>
           </div>
-          {/*<p>this is where the rotating foods are gonna be. it will be a random selection on a caraousel.</p>*/}
         </div>
+      </div>
     );
 }
 
