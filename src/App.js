d3a7c816d05e35fc0ee8a5d49f4e0a8cea7f6812
Marco <marco.a.romero0303@gmail.com>
2023-06-09 16:58:45 -0700

    created the template of how i want the ingredients page to be set up with some dummy text

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Navbar from "./components/navigation/NavBar";
 import Home from "./pages/Home";
 import FooterBar from "./components/footer/FooterBar";
+import IngredientsPage from './pages/IngredientPage';
 
 function App(){
   return (
     <React.Fragment>
       <Navbar />
-      <Home />
+      {/*<Home />*/}
+      <IngredientsPage />
       <FooterBar />
     </React.Fragment>
   );
