a36d65498b2a7bb0307750b8c3b58673ce5fb0a0
Marco Romero <46950374+mar807@users.noreply.github.com>
2023-09-05 11:41:17 -0700

    layout progress

---
+++ src/pages/DisplayFoods.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../styles/displayfoods/DisplayFoods.css"
+
+function DisplayFoods(){
+    return(
+        <div className="display-foods-main">
+            <div className="display-foods header">
+                <h4>All Foods You Stinkies</h4>
+            </div>
+            <div className="filter-and-food-section">
+                <div className="foods-filter-section">
+                    
+                </div>
+                <div className="all-foods-section">
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DisplayFoods;
