23afad3843dc81e301da5cb5b8d35f17ba8e9081
Marco <marco.a.romero0303@gmail.com>
2023-09-05 22:37:33 -0700

    adding routers to the web app. still working on kinks.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navigation/NavBar";
 import Home from "./pages/Home";
 import IngredientsPage from './pages/IngredientPage';
@@ -7,10 +8,15 @@ import DisplayFoods from './pages/DisplayFoods';
 function App(){
   return (
     <React.Fragment>
-      <Navbar />
-      {/*<Home />*/}
-      {/*<IngredientsPage />*/}
-      <DisplayFoods />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />} >
+            <Route index element={<Home />} />
+            <Route path="ingredients-page" element={<IngredientsPage />} />
+            <Route path="food-display-page" element={<DisplayFoods />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </React.Fragment>
   );
 }
