480fc19aaf8983ea660e6f20795c6682ff1885b7
Marco <marco.a.romero0303@gmail.com>
2023-09-11 22:44:43 -0700

    finally working on api related stuff. setting up component for food displays

---
+++ src/components/displayfoodspage/FoodDisplayPics.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import "../../styles/FoodDisplayPics.css";
+
+function FoodDisplayPics(){
+    // const [products, setProducts] = useState([]);
+    
+    // useEffect(() => {
+    //     fetch('api file')
+    //     .then((response) => response.json())
+    //     .then((data) => setProducts(data))
+    //     .catch((error) => console.error("Error fetching data: ", error));
+    // }, []);
+
+    return(
+        <h4>test</h4>
+    );
+}
+
+export default FoodDisplayPics;
