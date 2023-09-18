d66c47e57f53210f513bb4a0b1538a926187c990
Marco <marco.a.romero0303@gmail.com>
2023-09-17 19:21:08 -0700

    messing with api call stuff.

--- src/components/displayfoodspage/FoodDisplayPics.js
+++ src/components/displayfoodspage/FoodDisplayPics.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from "react";
 import "../../styles/FoodDisplayPics.css";
 
 function FoodDisplayPics(){
-    // const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
+    const apiKey = process.env.REACT_APP_API_KEY;
     
-    // useEffect(() => {
-    //     fetch('api file')
-    //     .then((response) => response.json())
-    //     .then((data) => setProducts(data))
-    //     .catch((error) => console.error("Error fetching data: ", error));
-    // }, []);
+    useEffect(() => {
+        fetch(`https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=${apiKey}`)
+        .then((response) => response.json())
+        .then((data) => setProducts(data))
+        .catch((error) => console.error("Error fetching data: ", error));
+    }, []);
     
     // this is the api url : https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=API-KEY
     // replace api key with what got emailed to me.
