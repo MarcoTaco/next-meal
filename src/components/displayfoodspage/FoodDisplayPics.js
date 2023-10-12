99609d095246d847e26b11f67239ec8f6cbf3b4f
Marco <marco.a.romero0303@gmail.com>
2023-10-12 15:06:12 -0700

    more api updates on different pages

--- src/components/displayfoodspage/FoodDisplayPics.js
+++ src/components/displayfoodspage/FoodDisplayPics.js
@@ -5,15 +5,12 @@ function FoodDisplayPics(){
     const [products, setProducts] = useState([]);
     const apiKey = process.env.REACT_APP_API_KEY;
     
-    useEffect(() => {
-        fetch(`https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=${apiKey}`)
-        .then((response) => response.json())
-        .then((data) => setProducts(data))
-        .catch((error) => console.error("Error fetching data: ", error));
-    }, []);
-    
-    // this is the api url : https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=API-KEY
-    // replace api key with what got emailed to me.
+    // useEffect(() => {
+    //     fetch(`https://api.spoonacular.com/food/products/search?query=yogurt&apiKey=${apiKey}`)
+    //     .then((response) => response.json())
+    //     .then((data) => setProducts(data))
+    //     .catch((error) => console.error("Error fetching data: ", error));
+    // }, []);
 
     return(
         <h4>test</h4>
--- src/pages/DisplayFoods.js
+++ src/pages/DisplayFoods.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/displayfoods/DisplayFoods.css"
 import Layout from "../components/Layout";
+//import { motion } from 'framer-motion';
+import { Link, useParams } from 'react-router-dom';
+
 
 function DisplayFoods(){
+    const [cuisine, setCuisine] = useState([]);
+    let params = useParams();
+    const apiKey = process.env.REACT_APP_API_KEY;
+
+    // this will grab food from api depending on which type of cuisine we pick
+    const getCuisine = async (name) => {
+        const data = await fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&cuisine=${name}`);
+        const recipes = await data.json();
+        setCuisine(recipes.results);
+    };
+
+    useEffect(() => {
+        //getCuisine(params.type);
+        console.log(params.type);
+    },[params.type]);
+    
     return(
         <Layout>
             <div className="display-foods-main">
